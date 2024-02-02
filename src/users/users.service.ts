44a50cf700316ef785e9ac2ba266b0fa1f3b06ea
username <useremail@qq.com>
2024-02-02 12:54:35 +0800

    update: a restful api for employees

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,47 +1,56 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDTO } from './dto/create-user.dto';
 import { UpdateUserDTO } from './dto/update-user.dto';
+import { NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class UsersService {
   private users = [
-    { "id": 1, "name": 'John Doe', "email": 'Since@123.biz', "role": 'INTERN' },
-    { "id": 2, "name": 'Jane Doe', "email": 'Shanem@123.com', "role": 'INTERN' },
-    { "id": 3,"name": 'Shane Doe', "email": 'shane@123.com', "role": 'ENGINEER' },
-    { "id": 4, "name": 'Shane Bush', "email": 'bush@123.com', "role": 'ADMIN'},
-    { "id": 5, "name": 'Shee Bush', "email": 'test@q13.com`', "role": 'ADMIN'}
+    { id: 1, name: 'John Doe', email: 'Since@123.biz', role: 'INTERN' },
+    { id: 2, name: 'Jane Doe', email: 'Shanem@123.com', role: 'INTERN' },
+    { id: 3, name: 'Shane Doe', email: 'shane@123.com', role: 'ENGINEER' },
+    { id: 4, name: 'Shane Bush', email: 'bush@123.com', role: 'ADMIN' },
+    { id: 5, name: 'Shee Bush', email: 'test@q13.com`', role: 'ADMIN' },
   ];
 
   findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
-    if(role) {
-        return this.users.filter(user => user.role === role);
+    if (role) {
+      const rolesArrary =  this.users.filter((user) => user.role === role);
+      if (rolesArrary.length === 0) {
+        throw new NotFoundException(`User with role ${role} not found`);
+      }
+      return rolesArrary;
     }
     return this.users;
   }
 
   findOne(id: number) {
-    return this.users.find(user => user.id === id);
+    const user = this.users.find((user) => user.id === id);
+    if (!user) {
+      throw new NotFoundException(`User with id ${id} not found`);
+    }
+    return user;
   }
 
   create(createUserDTO: CreateUserDTO) {
-    const usersByHighestId = this.users.sort((a, b) => b.id - a.id); 
-    const newUser = {id: usersByHighestId[0].id + 1, ...createUserDTO};  
+    const usersByHighestId = this.users.sort((a, b) => b.id - a.id);
+    const newUser = { id: usersByHighestId[0].id + 1, ...createUserDTO };
     this.users.push(newUser);
-    return {newUser};
-    }
+    return { newUser };
+  }
 
-   update(id: number, userUpdateDTO: UpdateUserDTO) {
-        this.users = this.users.map(user => {
-            if(user.id === id) {
-                return { ...user, ...userUpdateDTO };
-            }
-            return user;
-        })
-        return this.findOne(id);
-    }
+  update(id: number, userUpdateDTO: UpdateUserDTO) {
+    this.users = this.users.map((user) => {
+      if (user.id === id) {
+        return { ...user, ...userUpdateDTO };
+      }
+      return user;
+    });
+    return this.findOne(id);
+  }
 
-    delete(id: number) {
-        this.users = this.users.filter(user => user.id !== id);
-        return { id };
-    }
-}   
+  delete(id: number) {
+    this.users = this.users.filter((user) => user.id !== id);
+    return { id };
+  }
+}
